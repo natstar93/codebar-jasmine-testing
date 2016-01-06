beb260830237f51431129d48ef6105686d7f3789
Natalie Akam <natalie.akam@gmail.com>
2016-01-06 20:59:45 +0000

    Adds tests for temperatures and weights

--- spec/ConverterSpec.js
+++ spec/ConverterSpec.js
@@ -1,10 +1,36 @@
 describe("Cooking Ingredients Converter", function() {
 
   describe("Temperatures", function() {
-    it("Fahrenheit to Celsius", function() {
+    it("converts 100 Fahrenheit to 37.8 Celsius", function() {
       var result = convertFahrenheitToCelsius(100);
 
       expect(result).toEqual('37.8');
     });
+
+    it("converts 50 Fahrenheit to 10 Celsius", function() {
+      var result = convertFahrenheitToCelsius(50);
+
+      expect(result).toEqual('10.0');
+    });
+
+    it("converts 250 celsius to 482 fahrenheit ", function(){
+      var result = convertCelsiusToFahrenheit(250);
+
+      expect(result).toEqual('482.0');
+
+    });
+
   });
+
+  describe("Weight", function() {
+
+    it("converts 1 Pounds to 0.4 Kilos", function(){
+      var result = convertPoundsToKilos(1);
+
+      expect(result).toEqual('0.45');
+
+    });
+  });
+
+
 });
--- src/Converter.js
+++ src/Converter.js
@@ -1,4 +1,15 @@
 /** Unit Converter **/
 function convertFahrenheitToCelsius(temperature) {
-  //conversion code
+  var celsius = (temperature - 32)/1.8
+  return celsius.toFixed(1);
+};
+function convertCelsiusToFahrenheit(temp){
+  var fehrenheit = (temp * 1.8 + 32)
+
+  return fehrenheit.toFixed(1);
+};
+function convertPoundsToKilos(pound){
+  var kilos = (pound * 0.4536)
+
+  return kilos.toFixed(2);
 }
